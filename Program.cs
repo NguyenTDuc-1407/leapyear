81d528e63fcc851cbbde9222582e956ab9e7c688
Duc.NT <311020ntd@gmail>
2024-08-07 21:40:49 +0700

    leap year

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿int year;
 Console.WriteLine("Enter a year: ");
 year = Convert.ToInt32(Console.ReadLine());
-if (year %4 ==0 || year%100 ==0 || year%400==0)
+if (year %4 ==0 && year%100 ==0 && year%400==0)
 {
     Console.WriteLine("{0} is a leap year", year);
 }else
